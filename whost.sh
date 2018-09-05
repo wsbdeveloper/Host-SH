971e226abe1f6769cd3026351f4bbaad7cf8cf02
Wellington Bezerra <37592985+WSBgit@users.noreply.github.com>
2018-09-05 10:51:27 -0300

    Update Script

--- whost.sh
+++ whost.sh
@@ -7,9 +7,9 @@ echo -e "Exemplo uso da Ferramenta :"
 echo -e "/DesecParse.sh www.alvo.com.br\n"
 echo -e "<=== #################################### ===>\n"
 else
-mkdir -p -v /Documentos/DesecParsing/$1/
-wget -c -q -P /Documentos/DesecParsing/$1/ $1 
-cd /Documentos/DesecParsing/$1
+mkdir -p -v /Documentos/Parsing/$1/
+wget -c -q -P /Documentos/Parsing/$1/ $1 
+cd /Documentos/Parsing/$1
 cat index.html | grep "http://" | cut -d "/" -f 3 | grep ".com" | cut -d '"' -f 1 > List
 echo "| +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ |"
 echo "Resolvendo Hosts"
